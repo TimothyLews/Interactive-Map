d83784a855d0ada89f7e48f90763f54cf4796ac2
TimothyLewis <timlew2003@gmail.com>
2024-10-15 17:13:17 -0700

    proper planning for app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+// Initialize the map
+
+// Retrieve coordinates using the Geolocation API
+
+// Fetch business data from Foursquare
+
+// Handle and format Foursquare data
+
+// Set up event listeners
+// Trigger on window load
+
+// Handle business type selection submission
